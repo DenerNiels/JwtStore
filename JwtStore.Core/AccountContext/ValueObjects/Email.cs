1dd928e0391b02bc3a35e458907ab4bac5b645d6
Dener Niels <ksin_niels@outlook.com>
2024-02-17 11:19:44 +0000

    String extensions

---
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,18 @@
+﻿using JwtStore.Core.SharedContext.Extensions;
+using JwtStore.Core.SharedContext.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core.AccountContext.ValueObjects
+{
+    public class Email : ValueObject
+    {
+        private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+        public string Address { get; }
+        public string Hash => Address.ToBase64();
+    }
+}
