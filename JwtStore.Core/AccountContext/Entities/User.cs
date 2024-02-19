25021e203c7035909d6059f5addfa550870ee564
Dener Niels <ksin_niels@outlook.com>
2024-02-19 07:36:16 +0000

    finalizing the user entity

--- JwtStore.Core/AccountContext/Entities/User.cs
+++ JwtStore.Core/AccountContext/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using JwtStore.Core.SharedContext.Entities;
+﻿using JwtStore.Core.AccountContext.ValueObjects;
+using JwtStore.Core.SharedContext.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,36 @@ namespace JwtStore.Core.AccountContext.Entities
 {
     public class User : Entity
     {
+        protected User() { }
+        public User(string email, string? password = null)
+        {
+            Email = email;
+            Password = new Password(password);
+        }
+
+        public string Name { get; private set; } = string.Empty;
+        public Email Email { get; private set; } = null!;
+        public Password Password { get; private set; } = null!;
+        public string Image { get; private set; } = string.Empty;
+
+        public void UpdatePassword(string plainTextPassword, string code)
+        {
+            if (!string.Equals(code.Trim(), Password.ResetCode.Trim(), StringComparison.OrdinalIgnoreCase))
+                throw new Exception("Código de restauração inválido");
+            var password = new Password(plainTextPassword);
+            Password = password;
+        }
+
+        public void UpdateEmail(Email email)
+        {
+            Email = email;
+        }
+
+        public void ChancePassword(string plainTextPassword)
+        {
+            var password = new Password(plainTextPassword);
+            Password = password;
+        }
 
     }
 }
---
+++ JwtStore.Core/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Core
+{
+    public static  class Configuration
+    {
+
+        public static SecretsOnConfiguration Secrets { get; set; } = new();
+
+        public class SecretsOnConfiguration
+        {
+            public string ApiKey { get; set; } = string.Empty;
+            public string JwtPrivateKey { get; set; } = string.Empty;
+            public string PasswordSaltKey { get; set; } = string.Empty;
+        }
+    }
+}
