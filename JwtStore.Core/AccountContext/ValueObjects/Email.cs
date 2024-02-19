33c702f9b0e94d749447bab5087c12674f67df8d
Dener Niels <ksin_niels@outlook.com>
2024-02-19 05:58:17 +0000

    VL de email

--- JwtStore.Core/AccountContext/ValueObjects/Email.cs
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -4,15 +4,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace JwtStore.Core.AccountContext.ValueObjects
 {
-    public class Email : ValueObject
+    public partial class Email : ValueObject
     {
         private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
 
+        public Email(string address)
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new Exception("Email inválido");
+
+            Address = address.Trim().ToLower();
+
+            if (Address.Length < 5)
+                throw new Exception("Email inválido");
+
+            if (!EmailRegex().IsMatch(address))
+                throw new Exception("Email inválido");
+        }
+
         public string Address { get; }
         public string Hash => Address.ToBase64();
+
+        public static implicit operator string(Email email)
+            => email.ToString();
+
+        public static implicit operator Email(string address)
+            => new Email(address);
+
+        public override string ToString()
+            => Address;
+        
+
+        [GeneratedRegex(Pattern)]
+        private static partial Regex EmailRegex();
     }
 }
