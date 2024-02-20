2466138f78204e03df110c270976cdc064832854
Dener Niels <ksin_niels@outlook.com>
2024-02-20 06:22:12 +0000

    Response

--- JwtStore.Core/Contexts/AccountContext/UseCases/Create/Response.cs
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Response.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Flunt.Notifications;
+using JwtStore.Core.Contexts.SharedContext.UseCases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create
 {
-    public class Response
+    public class Response : SharedContext.UseCases.Response
     {
+        protected Response()
+        {
+        }
+
+        public Response(string message,
+            int status,
+            IEnumerable<Notification>? notifications = null)
+        {
+            Message = message;
+            Status = status;
+            Notifications = notifications;
+        }
+
+        public Response(string message, ResponseData data)
+        {
+            Message= message;
+            Status = 201;
+            Notifications = null;
+            Data = data;
+        }
+
+        public ResponseData? Data { get; set; }
+    }
+
+    public record ResponseData(Guid Id, string Name, string Email)
+    {
+
     }
 }
