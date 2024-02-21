303a08f7af7dd8fefe3ee396f50a44e9f994bdec
Dener Niels <ksin_niels@outlook.com>
2024-02-21 05:37:24 +0000

    Implementação do repositório

---
+++ JwtStore.Infra/Contexts/AccountContext/Mappings/UseCases/Create/Service.cs
@@ -0,0 +1,18 @@
+﻿using JwtStore.Core.Context.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Infra.Contexts.AccountContext.Mappings.UseCases.Create
+{
+    public class Service : IService
+    {
+        public Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
