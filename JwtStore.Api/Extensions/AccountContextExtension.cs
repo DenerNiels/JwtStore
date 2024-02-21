905c98966cfb9d70a8fa8e349a45b789cb9b0c09
Dener Niels <ksin_niels@outlook.com>
2024-02-21 06:19:38 +0000

    Atualizando Api

---
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace JwtStore.Api.Extensions
+{
+    public static class AccountContextExtension
+    {
+        public static void AddAccountContext(this WebApplicationBuilder builder)
+        {
+
+        }
+
+        public static void MapAccountEndponints(this WebApplication app)
+        {
+
+        }
+    }
+}
