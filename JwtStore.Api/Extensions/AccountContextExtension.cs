44477f2f79871499352ac5a9f710ff6bf88971a0
Dener Niels <ksin_niels@outlook.com>
2024-02-22 19:27:59 +0000

    Authenticate Repository

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -16,6 +16,12 @@ namespace JwtStore.Api.Extensions
                 JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IService,
                 JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Service>();
             #endregion
+
+            #region Authenticate
+            builder.Services.AddTransient<
+                JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Contracts.IRepository,
+                JwtStore.Infra.Contexts.AccountContext.UseCases.Authenticate.Repository>();
+            #endregion
         }
 
         public static void MapAccountEndponints(this WebApplication app)
@@ -30,6 +36,20 @@ namespace JwtStore.Api.Extensions
                 var result = await handler.Handle(request, new CancellationToken());
                 return result.IsSuccess
                     ? Results.Created("api/v1/users", result)
+                    : Results.Json(result, statusCode: result.Status);
+            });
+            #endregion
+
+            #region Authenticate
+            app.MapPost("api/v1/authenticate", async (
+                JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Request request,
+                IRequestHandler<
+                    JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Request,
+                    JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Response> handler) =>
+            {
+                var result = await handler.Handle(request, new CancellationToken());
+                return result.IsSuccess
+                    ? Results.Ok(result)
                     : Results.Json(result, statusCode: result.Status);
             });
             #endregion
---
+++ JwtStore.Infra/Contexts/AccountContext/UseCases/Authenticate/Repository.cs
@@ -0,0 +1,27 @@
+﻿using JwtStore.Core.Context.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Authenticate.Contracts;
+using JwtStore.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Authenticate
+{
+    public class Repository : IRepository
+    {
+        private readonly AppDbContext _context;
+
+        public Repository(AppDbContext context) =>context = _context;
+        
+        public async Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken)
+        {
+            return await _context
+                .Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Email.Address == email, cancellationToken);
+        }
+    }
+}
